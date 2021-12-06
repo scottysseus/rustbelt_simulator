7d86b2a46edef601d2422abf2eae7e42c4f42011
libjared <libjared@users.noreply.github.com>
2021-12-06 12:57:02 -0500

    Enable new JSX transform

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,10 @@ module.exports = {
     'standard',
     'standard-jsx',
     'standard-react'
-  ]
+  ],
+  rules: {
+    // next 2 lines: React 17's new JSX transform doesn't require `React`
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off'
+  }
 }
