ea81d3d8b0c7e70be787782f40a7b03e3f5704c6
Scott Weidenkopf <sweidenkopf@gmail.com>
2021-12-06 13:09:41 -0500

    addressed jared's feedback on the common webpack config

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -2,9 +2,6 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-const babelPresetEnv = require.resolve('@babel/preset-env')
-const babelPresetReact = require.resolve('@babel/preset-react')
-const babelPresetTypescript = require.resolve('@babel/preset-typescript')
 module.exports = env => {
   return {
     entry: './src/index.tsx',
@@ -16,7 +13,7 @@ module.exports = env => {
       rules: [
         {
           test: /\.(js|jsx|ts|tsx)$/,
-          exclude: /nodeModules/,
+          exclude: /node_modules/,
           use: [{
             loader: 'babel-loader'
           }]
@@ -32,7 +29,7 @@ module.exports = env => {
       new MiniCssExtractPlugin()
     ],
     resolve: {
-      extensions: ['*', '.js', '.jsx', '.tsx', '.ts']
+      extensions: ['.js', '.jsx', '.tsx', '.ts']
     }
   }
 }
