4ac53210bcc31ff0c1409be51043a9089d396a47
scottysseus <sweidenkopf@gmail.com>
2021-12-15 20:09:40 -0500

    more menu styling

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 export const Menu = React.forwardRef((props, ref) => {
   return (
     <div style={props.style} ref={ref} className='menu-main'>
-      <Stack spacing={2} className='menu-main-contents'>
-        Rustfield Revival
+      Rustfield Revival
+      <Stack spacing={2} className='menu-main-buttons'>
         <Button variant='contained' onClick={props.onStart}>Start</Button>
         <Button variant='contained'>Credits</Button>
       </Stack>
