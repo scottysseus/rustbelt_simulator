d8eee0c7d58cd4c1edef0850e25aa3ca12c0a3c0
libjared <libjared@users.noreply.github.com>
2021-12-16 18:49:54 -0500

    Color hovered `MapLocation`s

--- src/components/view/MapLocation.tsx
+++ src/components/view/MapLocation.tsx
@@ -1,6 +1,7 @@
 
 import { useLoader } from '@react-three/fiber'
-import { useState } from 'react'
+import { useRef } from 'react'
+import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { Tile } from '../../game_logic/interfaces'
 
@@ -10,32 +11,60 @@ import { Tile } from '../../game_logic/interfaces'
  * @returns
  */
 export function MapLocation (props: {row: number, column: number, gridInterval: number, tile: Tile, selected: boolean, onSelected: () => void}) {
-  // This reference gives us direct access to the THREE.Mesh object
-  // Hold state for hovered and clicked events
-  const [hovered, setHover] = useState(false)
-  // Return the view, these are regular Threejs elements expressed in JSX
+  const ref = useRef<THREE.Group>(null)
 
   const x = props.gridInterval * props.column
   const z = props.gridInterval * (props.row + 1)
-  const y = 0
 
   const gltf = useLoader(GLTFLoader, props.tile.definition.modelPath)
 
   const onPointerOver = (event: THREE.Event) => {
-    event.stopPropagation()
-    setHover(true)
+    console.log(event)
+    if (ref.current != null) {
+      setColorRecursive(ref.current, new THREE.Color(0x663399))
+    }
   }
   const onPointerOut = (event: THREE.Event) => {
-    event.stopPropagation()
-    setHover(false)
+    if (ref.current != null) {
+      setColorRecursive(ref.current, null)
+    }
   }
 
-  const onClick = (event) => {
+  const onClick = (event: THREE.Event) => {
     console.log('Clicked on Map Location', props.tile.definition.name, props.row, props.column)
     props.onSelected()
   }
 
   return (
-    <primitive object={gltf.scene} position={[x, y, z]} onClick={onClick} />
+    <primitive
+      ref={ref}
+      object={gltf.scene}
+      position={[x, 0, z]}
+      onClick={onClick}
+      onPointerOver={onPointerOver}
+      onPointerOut={onPointerOut}
+    />
   )
+}
+
+function setColorRecursive (object3d: THREE.Object3D, color: THREE.Color | null) {
+  if (object3d instanceof THREE.Mesh) {
+    const mesh = object3d as THREE.Mesh
+    if (!(mesh.material instanceof THREE.MeshStandardMaterial)) {
+      console.warn('Found wrong material type:', mesh.material.constructor.name)
+      return
+    }
+    const material = mesh.material
+    if (!material.userData.originalColor) {
+      material.userData.originalColor = material.color
+    }
+    if (!color) {
+      material.color = material.userData.originalColor
+    } else {
+      material.color = color
+    }
+  }
+  for (const child of object3d.children) {
+    setColorRecursive(child, color)
+  }
 }
