f93c316936cd4fca8496923d3b5d17f9501e77f8
Scott Weidenkopf <sweidenkopf@gmail.com>
2021-12-21 21:18:09 -0500

    fixed contract list item spacing

--- src/components/hud/ContractPane.tsx
+++ src/components/hud/ContractPane.tsx
@@ -1,20 +1,24 @@
-import { Card, Stack, Typography } from '@mui/material'
-import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentDissatisfied'
+import { Card, Collapse, List, ListItem, Stack, Typography } from '@mui/material'
 import { Contract } from '../../game_logic'
 import { catalog } from '../../data/contract-catalog'
+import { TransitionGroup } from 'react-transition-group'
 
 function contractItem (props: {contract: Contract}) {
   return (
-    <Card key={props.contract.name} className='contract-card contract-list-item'>
-      <div className='contract-card-heading'>
-        <Typography sx={{ fontSize: 'h6.fontSize' }}>{props.contract.name}</Typography>
-        <Typography variant='caption'>
-          {/* TODO devise a way to consistently display rewards */}
-          {props.contract.reward}
-        </Typography>
-      </div>
-      <Typography>{props.contract.description}</Typography>
-    </Card>
+    <Collapse className='contract-list-item-collapse' key={props.contract.name}>
+      <ListItem className='contract-list-item-collapse'>
+        <Card className='contract-card'>
+          <div className='contract-card-heading'>
+            <Typography sx={{ fontSize: 'h6.fontSize' }}>{props.contract.name}</Typography>
+            <Typography variant='caption'>
+              {/* TODO devise a way to consistently display rewards */}
+              {props.contract.reward}
+            </Typography>
+          </div>
+          <Typography>{props.contract.description}</Typography>
+        </Card>
+      </ListItem>
+    </Collapse>
   )
 }
 
@@ -22,8 +26,10 @@ export function ContractPane () {
   const contracts = Object.keys(catalog).map(contractId => contractItem({ contract: catalog[contractId] }))
 
   return (
-    <Stack className='contract-pane pane contract-list' spacing={2}>
-      {contracts}
-    </Stack>
+    <List className='contract-pane pane contract-list' spacing={2}>
+      <TransitionGroup>
+        {contracts}
+      </TransitionGroup>
+    </List>
   )
 }
--- src/data/contract-catalog.ts
+++ src/data/contract-catalog.ts
@@ -1,19 +1,25 @@
 import { ContractCatalog } from '../game_logic'
 
+export const contractIDs = {
+  imLovinIt: 'i\'m lovin\' it',
+  greenTourism: 'green tourism',
+  brokenWindowsTheory: 'broken windows theory'
+}
+
 export const catalog:ContractCatalog = {
-  'i\'m lovin\' it': {
+  [contractIDs.imLovinIt]: {
     name: 'I\'m Lovin\' It',
     description: 'Build at least one drive thru restaurant',
     completed: false,
     reward: '$100/turn, 🙂3'
   },
-  'green tourism': {
+  [contractIDs.greenTourism]: {
     name: 'Green Tourism',
     description: 'Build at least 1 park',
     completed: false,
     reward: '$85, 🙂5/turn'
   },
-  'broken windows theory': {
+  [contractIDs.brokenWindowsTheory]: {
     name: 'Broken Windows Theory',
     description: 'Improve each property at least once',
     completed: false,
