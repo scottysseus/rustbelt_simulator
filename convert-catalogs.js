9b2bee6ff56ab4dcad9f838335de6c0a35091119
nmoadev <alain.kuchta@gmail.com>
2021-12-25 15:12:11 -0500

    Add updated tile and project catalog
    w/ script to generate from CSV downloaded form drive

---
+++ convert-catalogs.js
@@ -0,0 +1,66 @@
+const { parse } = require('csv-parse/sync')
+const { readFile, writeFile } = require('fs/promises')
+
+async function convertTileCatalog () {
+  const csvTileCatalog = await readFile('./Rustfield Catalog - Tiles.csv')
+  const tileCat = parse(csvTileCatalog, { columns: true })
+  const finalCatalog = {}
+  for (const entry of tileCat) {
+    entry.projects = []
+    if (entry.project1) {
+      entry.projects.push(entry.project1)
+    }
+    if (entry.project2) {
+      entry.projects.push(entry.project2)
+    }
+    if (entry.project3) {
+      entry.projects.push(entry.project3)
+    }
+    entry.tags = entry.tags.split(',')
+    entry.happiness = parseInt(entry.happiness)
+    entry.revenue = parseInt(entry.revenue)
+    entry.happinessGrowth = parseInt(entry.happinessGrowth)
+    delete entry.project1
+    delete entry.project2
+    delete entry.project3
+    delete entry['Modeling Status']
+    delete entry['GLTF Export Status']
+    delete entry['Export Instructions']
+    delete entry['happiness growth']
+    finalCatalog[entry.id] = entry
+  }
+  const catalogFile = `
+import { TileCatalog } from '../game_logic'
+
+// This file implements a "database" of tiles, each with unique appearance, choices, etc
+export const tileCatalog: TileCatalog = ${JSON.stringify(finalCatalog, undefined, 2)}`
+  await writeFile('src/data/tile-catalog.ts', catalogFile)
+}
+
+async function convertProjectCatalog () {
+  await convertTileCatalog()
+
+  const csvProjectCatalog = await readFile('./Rustfield Catalog - Projects.csv')
+  const projectCat = parse(csvProjectCatalog, { columns: true })
+  const finalCat = {}
+  for (entry of projectCat) {
+    entry.cost = parseInt(entry.cost)
+    entry.effort = parseInt(entry.effort)
+    finalCat[entry.id] = entry
+  }
+  console.log(projectCat)
+  console.log(finalCat)
+
+  const projectCatalogFile = `
+import { ProjectCatalog } from '../game_logic'
+// This file implements a "database" of tiles, each with unique appearance, choices, etc
+export const projectCatalog: ProjectCatalog = ${JSON.stringify(finalCat, undefined, 2)}`
+  await writeFile('src/data/project-catalog.ts', projectCatalogFile)
+}
+
+async function convert () {
+  await convertTileCatalog()
+  await convertProjectCatalog()
+}
+
+convert().catch(console.log)
--- src/data/project-catalog.ts
+++ src/data/project-catalog.ts
@@ -1,26 +1,189 @@
-import { ProjectCatalog } from '../game_logic'
 
+import { ProjectCatalog } from '../game_logic'
 // This file implements a "database" of tiles, each with unique appearance, choices, etc
 export const projectCatalog: ProjectCatalog = {
-  // 'repair-firehouse': {
-  //   name: 'Repair',
-  //   description: 'Fix it up!',
-  //   targetTileType: 'firehouse-1',
-  //   cost: 300,
-  //   effort: 9
-  // },
   demolish: {
+    id: 'demolish',
     name: 'Demolish',
-    description: 'Tear it down!',
+    description: 'lorem ipsum',
     targetTileType: 'empty',
     cost: 500,
-    effort: 20
+    effort: 10
   },
-  refine: {
-    name: 'Refine',
-    description: 'Honestly, nothing.',
-    targetTileType: 'empty',
-    cost: 0,
-    effort: 1
+  'repair-gas': {
+    id: 'repair-gas',
+    name: 'Repair',
+    description: 'lorem ipsum',
+    targetTileType: 'gas-1',
+    cost: 340,
+    effort: 4
+  },
+  'repair-park': {
+    id: 'repair-park',
+    name: 'Cleanup',
+    description: 'lorem ipsum',
+    targetTileType: 'park-1',
+    cost: 75,
+    effort: 3
+  },
+  'upgrade-park-dog': {
+    id: 'upgrade-park-dog',
+    name: 'Dogs!',
+    description: 'lorem ipsum',
+    targetTileType: 'park-dog',
+    cost: 200,
+    effort: 2
+  },
+  'upgrade-park-memorial': {
+    id: 'upgrade-park-memorial',
+    name: 'Commerate',
+    description: 'lorem ipsum',
+    targetTileType: 'park-mem',
+    cost: 300,
+    effort: 7
+  },
+  'upgrade-park-sports': {
+    id: 'upgrade-park-sports',
+    name: 'Sports',
+    description: 'lorem ipsum',
+    targetTileType: 'park-sports',
+    cost: 150,
+    effort: 8
+  },
+  'build-park': {
+    id: 'build-park',
+    name: 'Make a park',
+    description: 'lorem ipsum',
+    targetTileType: 'park-1',
+    cost: 250,
+    effort: 6
+  },
+  'repair-house': {
+    id: 'repair-house',
+    name: 'Repair',
+    description: 'lorem ipsum',
+    targetTileType: 'house-1',
+    cost: 100,
+    effort: 5
+  },
+  'upgrade-house-family': {
+    id: 'upgrade-house-family',
+    name: 'Make it bigger',
+    description: 'lorem ipsum',
+    targetTileType: 'house-2',
+    cost: 286,
+    effort: 7
+  },
+  'repair-shop': {
+    id: 'repair-shop',
+    name: 'Repair',
+    description: 'lorem ipsum',
+    targetTileType: 'shop-small',
+    cost: 392,
+    effort: 3
+  },
+  'upgrade-shop-market': {
+    id: 'upgrade-shop-market',
+    name: 'Market',
+    description: 'lorem ipsum',
+    targetTileType: 'shop-market',
+    cost: 289,
+    effort: 7
+  },
+  'upgrade-shop-supermarket': {
+    id: 'upgrade-shop-supermarket',
+    name: 'Supermarket',
+    description: 'lorem ipsum',
+    targetTileType: 'shop-super',
+    cost: 712,
+    effort: 12
+  },
+  'repair-library': {
+    id: 'repair-library',
+    name: 'Repair',
+    description: 'lorem ipsum',
+    targetTileType: 'library-1',
+    cost: 800,
+    effort: 15
+  },
+  'upgrade-library': {
+    id: 'upgrade-library',
+    name: 'Upgrade',
+    description: 'lorem ipsum',
+    targetTileType: 'library-2',
+    cost: 750,
+    effort: 4
+  },
+  'repair-firestation': {
+    id: 'repair-firestation',
+    name: 'Repair',
+    description: 'lorem ipsum',
+    targetTileType: 'fire-1',
+    cost: 800,
+    effort: 7
+  },
+  'upgrade-restaurant-family': {
+    id: 'upgrade-restaurant-family',
+    name: 'Family',
+    description: 'lorem ipsum',
+    targetTileType: 'restaurant-1',
+    cost: 870,
+    effort: 5
+  },
+  'upgrade-restaurant-fine': {
+    id: 'upgrade-restaurant-fine',
+    name: 'Fine',
+    description: 'lorem ipsum',
+    targetTileType: 'restaurant-2',
+    cost: 438,
+    effort: 8
+  },
+  'build-office-tower': {
+    id: 'build-office-tower',
+    name: 'Corporitize',
+    description: 'lorem ipsum',
+    targetTileType: 'office-1',
+    cost: 1000,
+    effort: 15
+  },
+  'build-wind': {
+    id: 'build-wind',
+    name: 'Build a Wind Plant',
+    description: 'lorem ipsum',
+    targetTileType: 'energy-wind',
+    cost: 950,
+    effort: 10
+  },
+  'convert-wind': {
+    id: 'convert-wind',
+    name: 'Convert to Wind',
+    description: 'lorem ipsum',
+    targetTileType: 'energy-wind',
+    cost: 1200,
+    effort: 25
+  },
+  'upgrade-office-2': {
+    id: 'upgrade-office-2',
+    name: 'Add a business',
+    description: 'lorem ipsum',
+    targetTileType: 'office-2',
+    cost: 250,
+    effort: 5
+  },
+  'upgrade-office-3': {
+    id: 'upgrade-office-3',
+    name: 'Add a business',
+    description: 'lorem ipsum',
+    targetTileType: 'office-3',
+    cost: 250,
+    effort: 5
+  },
+  'upgrade-office-4': {
+    id: 'upgrade-office-4',
+    name: 'Add a business',
+    description: 'lorem ipsum',
+    targetTileType: 'office-4',
+    cost: 250,
+    effort: 5
   }
 }
