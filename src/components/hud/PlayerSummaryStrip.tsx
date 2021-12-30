902c60959c4f3d0de38cdea2be309b30717fd50c
Scott Weidenkopf <sweidenkopf@gmail.com>
2021-12-30 15:19:16 -0500

    use an hourglass icon for the turn count

--- src/components/hud/PlayerSummaryStrip.tsx
+++ src/components/hud/PlayerSummaryStrip.tsx
@@ -27,7 +27,7 @@ export function PlayerSummaryStrip (props: {gameState: GameState}) {
         </Badge>
       </Typography>
       <Divider orientation='vertical' variant='middle' />
-      <Typography>↩️{props.gameState.game.turnCounter}</Typography>
+      <Typography>⌛{props.gameState.game.turnCounter}</Typography>
     </AppBar>
   )
 }
