1a96a7c052ab4b760ef8e9fa8aad56947975de58
nmoadev <alain.kuchta@gmail.com>
2021-12-31 12:05:48 -0500

    Add into text

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,31 @@ import { Fade } from '@mui/material'
 import { useState } from 'react'
 import { GameDisplay } from './components/GameDisplay'
 import { Menu } from './components/Menu'
+import { GameIntro } from './components/GameIntro'
 
 function App () {
-  const [inGame, setInGame] = useState(false)
+  const [menuState, setMenuState] = useState('menu')
 
   const onStart = () => {
-    setInGame(() => true)
+    setMenuState(() => 'intro')
   }
+
+  const onContinue = () => {
+    setMenuState(() => 'game')
+  }
+
+  // menu -> intro -> game
 
   return (
     <>
-      <Fade unmountOnExit appear={false} in={!inGame}>
+      <Fade unmountOnExit appear={false} in={menuState === 'menu'}>
         <Menu onStart={onStart} />
       </Fade>
-      <Fade in={inGame}>
-        <GameDisplay started={inGame} />
+      <Fade in={menuState === 'intro'}>
+        <GameIntro onContinue={onContinue} />
+      </Fade>
+      <Fade in={menuState === 'game'}>
+        <GameDisplay started={menuState === 'game'} />
       </Fade>
     </>
   )
