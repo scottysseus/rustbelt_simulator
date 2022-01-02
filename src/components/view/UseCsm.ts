632862d5956be3f51e3e09cb46ea0c78eb82f398
libjared <libjared@users.noreply.github.com>
2022-01-02 03:52:47 -0500

    Fallback to no-shadow lighting when GPU sux

--- src/components/view/UseCsm.ts
+++ src/components/view/UseCsm.ts
@@ -2,62 +2,85 @@ import * as THREE from 'three'
 import { useFrame, useThree } from '@react-three/fiber'
 import { useEffect, useRef } from 'react'
 import { CSM } from 'three/examples/jsm/csm/CSM'
+import { useDetectGPU } from '@react-three/drei'
 
 const LIGHT_DIRECTION = new THREE.Vector3(3, -7, -2).normalize()
 
 export default function useCsm () {
   const csmRef = useRef<CSM>()
+  const fallbackLightRef = useRef<THREE.DirectionalLight>()
   const scene = useThree(state => state.scene)
   const camera = useThree(state => state.camera)
+  const { tier } = useDetectGPU()
+
+  const mode = tier === 3 ? 'csm' : 'fallback'
 
   useEffect(() => {
-    csmRef.current = new CSM({
-      maxFar: 15,
-      cascades: 4,
-      // @ts-ignore enums are bad
-      mode: 'practical',
-      parent: scene,
-      shadowMapSize: 4096,
-      camera: camera,
-      lightDirection: LIGHT_DIRECTION,
-      lightIntensity: 1.0,
-      lightNear: 1,
-      lightFar: 40,
-      lightMargin: 6,
-      shadowBias: -0.0005
-    })
-    csmRef.current.fade = true
+    if (mode === 'csm') {
+      csmRef.current = new CSM({
+        maxFar: 15,
+        cascades: 4,
+        // @ts-ignore enums are bad
+        mode: 'practical',
+        parent: scene,
+        shadowMapSize: 4096,
+        camera: camera,
+        lightDirection: LIGHT_DIRECTION,
+        lightIntensity: 1.0,
+        lightNear: 1,
+        lightFar: 40,
+        lightMargin: 6,
+        shadowBias: -0.0005
+      })
+      csmRef.current.fade = true
+    } else if (mode === 'fallback') {
+      fallbackLightRef.current = new THREE.DirectionalLight(0xffffff, 1.0)
+      fallbackLightRef.current.position.copy(LIGHT_DIRECTION.clone().negate())
+      scene.add(fallbackLightRef.current)
+    }
 
+    // cleanups allow switching between modes
     return () => {
       if (csmRef.current) {
         csmRef.current.remove()
+        csmRef.current.dispose()
+        csmRef.current = undefined
+      }
+      if (fallbackLightRef.current) {
+        fallbackLightRef.current.removeFromParent()
+        fallbackLightRef.current.dispose()
+        fallbackLightRef.current = undefined
       }
     }
-  }, [camera, scene])
+  }, [camera, mode, scene])
 
+  // when in CSM mode, setup materials on every render,
+  // since we have to traverse the scene to find new materials anyway
   useEffect(() => {
     if (!csmRef.current) { return }
-    const csm = csmRef.current
-
-    scene.traverse((obj) => {
-      if (obj.name === 'mapLocation') {
-        obj.traverse((innerObj) => {
-          if (innerObj instanceof THREE.Mesh) {
-            csm.setupMaterial(innerObj.material)
-          }
-        })
-      }
-
-      if (obj.name === 'plainPlane') {
-        const material = (obj as THREE.Mesh).material as THREE.Material
-        csm.setupMaterial(material)
-      }
-    })
-  }, [scene])
+    setupMaterials(scene, csmRef.current)
+  })
 
   useFrame(() => {
     if (csmRef.current) {
       csmRef.current.update()
+    }
+  })
+}
+
+function setupMaterials (scene: THREE.Scene, csm: CSM) {
+  scene.traverse((obj) => {
+    if (obj.name === 'mapLocation') {
+      obj.traverse((innerObj) => {
+        if (innerObj instanceof THREE.Mesh) {
+          csm.setupMaterial(innerObj.material)
+        }
+      })
+    }
+
+    if (obj.name === 'plainPlane') {
+      const material = (obj as THREE.Mesh).material as THREE.Material
+      csm.setupMaterial(material)
     }
   })
 }
